7aec77a5d6bdddc7380cccd42e491aeb78a557d0
rifatrakib <Rakib.1508@outlook.com>
2023-10-08 17:20:24 +0600

    added foreign key column in the oauth table

--- server/models/users.py
+++ server/models/users.py
@@ -1,12 +1,13 @@
 from fastapi_users.db import SQLAlchemyBaseOAuthAccountTable, SQLAlchemyBaseUserTable
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from server.models import Base
 
 
 class OAuthAccount(SQLAlchemyBaseOAuthAccountTable, Base):
-    pass
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="cascade"), nullable=False)
 
 
 class User(SQLAlchemyBaseUserTable, Base):
-    oauth_accounts: Mapped[list[OAuthAccount]] = relationship(OAuthAccount, lazy="joined")
+    oauth_accounts: Mapped[list[OAuthAccount]] = relationship(OAuthAccount)
